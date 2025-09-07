3b715cc18b113ef252de2aed5755025fcda6fb62
Michael Dovoh <mdovoh@banquemisr.com>
2025-09-07 05:29:48 -0300

    Updated test module

--- feature/login/src/commonTest/kotlin/com/dovoh/android_mvi/feature/login/di/TestModule.kt
+++ feature/login/src/commonTest/kotlin/com/dovoh/android_mvi/feature/login/di/TestModule.kt
@@ -1,6 +1,5 @@
 package com.dovoh.android_mvi.feature.login.di
 
-
 import com.dovoh.android_mvi.core.auth.model.UserDomainModel
 import com.dovoh.android_mvi.core.common.Mapper
 import com.dovoh.android_mvi.core.network.auth.mapper.LoginResponseToDomainMapper
@@ -11,18 +10,17 @@ import com.dovoh.android_mvi.feature.login.fakes.FakeLoginUseCase
 import com.dovoh.android_mvi.feature.login.presentation.LoginViewModel
 import com.dovoh.android_mvi.feature.login.presentation.mapper.UserDomainToUiMapper
 import com.dovoh.android_mvi.feature.login.presentation.model.UserUiModel
-import org.koin.core.module.dsl.viewModel
 import org.koin.dsl.module
 
-val testModule = module(true) {
-
-    // Mappers
-
+val testModule = module {
+    // fakes
     single<LoginUseCase> { FakeLoginUseCase() }
+    single<AuthRepository> { FakeAuthRepository() }
 
+    // mappers
     single { LoginResponseToDomainMapper() }
     single<Mapper<UserDomainModel, UserUiModel>> { UserDomainToUiMapper() }
 
-    single<AuthRepository> { FakeAuthRepository() }
-    viewModel { LoginViewModel(get(), get()) }
+    // ViewModel as a factory (avoid koin-android viewModel DSL in commonTest)
+    factory { LoginViewModel(get(), get()) }
 }
